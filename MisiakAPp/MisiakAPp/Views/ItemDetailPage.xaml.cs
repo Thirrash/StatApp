98b152ada771e4a2ddd057e8b973523f6593d049
DESKTOP-C4PN4TF\Administrator <thirrasch@gmail.com>
2020-12-26 12:49:16 +0100

    Add solution and template project

---
+++ MisiakAPp/MisiakAPp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MisiakAPp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MisiakAPp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
